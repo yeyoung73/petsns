a1f2a165b46cfd62ad488787ca23839ccf9c7e39
leeyy <leeyy@ais-info.co.jp>
2025-07-15 14:14:54 +0900

    chore

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ import walkRoutes from "./routes/walk.js";
 const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://your-app.up.railway.app", // Railway 주소로 나중에 바꿔줘
+  "petsns-production.up.railway.app", // Railway 주소로 나중에 바꿔줘
 ];
 
 app.use(
