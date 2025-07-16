3067264f92e0854b1f7ab7ce97d1cac58f70ed17
leeyy <leeyy@ais-info.co.jp>
2025-07-16 12:47:28 +0900

    Fix Vite ES module configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -10,10 +11,10 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    host: true,
+    host: "0.0.0.0",
   },
   preview: {
     port: 3000,
-    host: true,
+    host: "0.0.0.0",
   },
 });
